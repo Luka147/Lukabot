b4952f7ad6b47cb02e9e38b980e812672471ae19
Luka <lukaxdwot@gmail.com>
2018-08-14 17:47:28 +0200

    puppy command

--- cmds/puppy.js
+++ cmds/puppy.js
@@ -13,10 +13,22 @@ module.exports = {
     ],
 	run: async (client, message) => {
     randomPuppy()
-    .then(url => {
-      message.channel.send( {
-        files: [url]
-      }).catch(console.error);
-    })
-  }
+      .then(url => {
+          const embed = new Discord.MessageEmbed()
+          .setColor(0xFF69B4)
+          .setTitle("Isn't it cute :heart_eyes:")
+          .setAuthor(message.author.username)
+          .setTimestamp();
+  
+          try {
+          message.channel.send(embed);
+          }
+          catch (e) {
+          message.reply(`Couldn't send message because of: ${e}`)
+          }
+
+          files: [url]
+        }).catch(console.error);
+      })
+    }
 };
