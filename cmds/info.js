0ade3a68b1ca27ac539d8bf96ee833852c2ef0a4
Xavinlol <38567507+Xavinlol@users.noreply.github.com>
2018-08-14 15:07:14 +0200

    Update info.js
    fixes and embed with all fields example

--- cmds/info.js
+++ cmds/info.js
@@ -11,7 +11,21 @@ module.exports = {
         }
     ],
 	run: async (client, message) => {
-    message.guild(guild.members.array().length);
-    
+          message.reply(`member cound: ${message.guild.members.array().length}`);
+	
+	  const embed = new Discord.MessageEmbed() //start the embed message template
+            .setTitle("title")
+            .setAuthor(message.author.username)
+            .setColor(0x6441a5)
+            .setDescription("description")
+            .setFooter("[LOOOL]", client.user.avatarURL({ format: "png" }))
+	    // picture as thumbnail
+            //.setThumbnail()
+            .setTimestamp()
+            .setURL(`www.discord.js.org`)
+            .addField("field1", "value1", false)
+            .addField("member count", message.guild.members.array().length, false)
+            .addField("field3", "value 3", true); //end the embed message template
+	  await message.channel.send(embed);
   }
 };
