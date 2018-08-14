141885431bd3168314f350d1556591a36a35113e
Luka <lukaxdwot@gmail.com>
2018-08-14 16:02:49 +0200

    Info command made, still need to change date format

--- cmds/info.js
+++ cmds/info.js
@@ -11,21 +11,18 @@ module.exports = {
         }
     ],
 	run: async (client, message) => {
-          message.reply(`member cound: ${message.guild.members.array().length}`);
-	
 	  const embed = new Discord.MessageEmbed() //start the embed message template
-            .setTitle("title")
+            .setTitle(":computer: "+ message.guild.name)
             .setAuthor(message.author.username)
             .setColor(0x6441a5)
-            .setDescription("description")
-            .setFooter("[LOOOL]", client.user.avatarURL({ format: "png" }))
-	    // picture as thumbnail
+            .setDescription("Created at: "+ message.guild.createdAt)
+            .setFooter("Lukabot", client.user.avatarURL({ format: "png" }))
+	        // picture as thumbnail
             //.setThumbnail()
             .setTimestamp()
-            .setURL(`www.discord.js.org`)
-            .addField("field1", "value1", false)
-            .addField("member count", message.guild.members.array().length, false)
-            .addField("field3", "value 3", true); //end the embed message template
+            .addField(":crown: Server owner: ", message.guild.owner, false)
+            .addField(":family: Member count: ", message.guild.members.array().length, false)
+            .addField(":medal: Roles:", message.guild.roles.array(), true); //end the embed message template
 	  await message.channel.send(embed);
   }
 };
