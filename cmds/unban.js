0a2298cadbe7e206a218a3d0614087c4c7b830ba
Xavin <programs@xavinlol.de>
2018-10-24 19:44:15 +0200

    example of args usage

--- cmds/unban.js
+++ cmds/unban.js
@@ -10,6 +10,15 @@ module.exports = {
             description: "Unbans a user"
         }
     ],
-	run: async (client, message,args) => {
-    }
-};
+  run: async (client, message,args) => {
+    let userID = args[1]
+    if (isEmpty(userID))
+      return message.reply("You gotta provide me the ID to use: \`!unban <ID>\`")
+
+    // here you have the potential id in userID
+  }
+}
+
+function isEmpty(str) {
+    return (!str || 0 === str.length);
+}
