4d6eb67b51a908c69179037585d281bbd0d49b18
Luka <lukaxdwot@gmail.com>
2018-10-26 20:34:16 +0200

    retarded cmd

--- cmds/retarded.js
+++ cmds/retarded.js
@@ -1,7 +1,8 @@
 const Discord = require("discord.js")
 var Jimp = require('jimp')
-const IMAGE_PATH = 'C:/Users/Luka/Desktop/'
-const TEMPLATE_FILE = '927.png'
+const IMAGET_PATH = 'D:/github/Lukabot/Templates/'
+const IMAGEG_PATH = 'D:/github/Lukabot/Generated/'
+const TEMPLATE_FILE = 'retarded-template.png'
 const NEW_FILE = 'retarded.png'
 const x  = 640
 const y = 640
@@ -24,7 +25,7 @@ module.exports = {
 
     text = text.trim() // trim removes spaces on the beginning and the end
 
-    let image = await Jimp.read(IMAGE_PATH + TEMPLATE_FILE).catch(e => {
+    let image = await Jimp.read(IMAGET_PATH + TEMPLATE_FILE).catch(e => {
         return console.log("couldnt read image" + e)
     })
 
@@ -36,13 +37,13 @@ module.exports = {
     })
 
     image.print(font, 350, 40, text, maxWidth);
-    await image.writeAsync(IMAGE_PATH + NEW_FILE).catch(e => {
+    await image.writeAsync(IMAGEG_PATH + NEW_FILE).catch(e => {
         return console.log("error while writing file: " + e)
     })
 
     message.channel.send({
       files: [{
-        attachment: IMAGE_PATH + NEW_FILE,
+        attachment: IMAGEG_PATH + NEW_FILE,
         name: NEW_FILE
        }]
     }).catch(console.error);     
